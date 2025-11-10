e017f9c9ed6af07c2af4a719f978f574e4cfa25f
Miguel Garrido <hi@miguel.nz>
2025-11-10 12:20:04 +0100

    readme improved

--- routes/reviews.js
+++ routes/reviews.js
@@ -2,7 +2,6 @@ import express from 'express';
 
 const router = express.Router();
 
-// Aquí van las rutas para los reviews
-// Los estudiantes implementarán las rutas en las iteraciones
+// Aquí se deben implementar las rutas para los reviews
 
 export default router;
